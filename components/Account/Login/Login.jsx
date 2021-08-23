d2025f69fe6d0ffe325cb89ec3baf37923f76504
Koushik Saha <sahakoushik@moveon.work>
2021-08-24 00:36:08 +0600

    add login form

--- components/Account/Login/Login.jsx
+++ components/Account/Login/Login.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
+import {useForm} from "react-hook-form";
 
 const Login = (Props) => {
 
+    const {register, handleSubmit} = useForm()
+
+    const onSubmit = (data) => {
+
+    }
+
     return (
-      <>
-      </>
+        <div className="login-wrap mvn-acc-form">
+            <div className="mvn-acc-form__title">
+                Log in to Strativ Quiz
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-label-group my-5">
+                    <input
+                        className="form-control"
+                        id="email_mobile"
+                        type="text"
+                        {...register('username', { required: true })}
+                        autoFocus
+                        required
+                    />
+                    <label className="form-control-placeholder" htmlFor="email_mobile">Email Address/Mobile
+                        Number</label>
+                </div>
+                <div className="form-label-group mt-5">
+                    <input
+                        id="password-login"
+                        className="form-control"
+                        type="password"
+                        name="password"
+                        {...register('password', { required: true })}
+                        required
+                    />
+                    <label className="form-control-placeholder" htmlFor="password-login">Password</label>
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Log in</button>
+            </form>
+        </div>
     )
 
 }
